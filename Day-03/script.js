4c17ce55f31bf39d85b55c3de138cf211805ed62
Professor McGreGor <professor1mcgregor366@gmail.com>
2024-10-30 21:38:32 +0600

    Create script.js

---
+++ Day-03/script.js
@@ -0,0 +1,57 @@
+let message = `hello world`;
+console.log(message);
+
+let someThing;
+someThing = 1;
+console.log(someThing);
+
+const pi = 3.1415;
+console.log(pi);
+
+let someMoreThing = [`hello`, `world`, `this`, `is`, `professor`, `mcgregor`];
+console.log(someMoreThing);
+console.log(someMoreThing[0]);
+console.log(someMoreThing[1]);
+console.log(someMoreThing[2]);
+console.log(someMoreThing[3]);
+console.log(someMoreThing[4]);
+console.log(someMoreThing[5]);
+
+let someMoreMoreThing = {
+   name: `lionel messi`,
+   age: 36,
+   profession: `footballer`,
+   country: `argentina`,
+   club: `inter miami`,
+   worldCup: true,
+   greatestFootballer: true,
+   legend: true,  
+}
+console.log(someMoreMoreThing);
+console.log(someMoreMoreThing.name);
+console.log(someMoreMoreThing.age);
+console.log(someMoreMoreThing.country);
+console.log(someMoreMoreThing.club);
+console.log(someMoreMoreThing.worldCup);
+console.log(someMoreMoreThing.greatestFootballer);
+console.log(someMoreMoreThing.legend);
+
+const someMoreMoreMoreThing = [
+   {
+      name: `Professor McGreGor`,
+      age: 30,
+      profession:[killer, developer, businessman],
+      statusIn: `future`,
+      married: false,
+      fight: true,
+      idols: {
+         name: `Muhammad Ali`,
+         name2: `Bruce Lee`,
+         name3: `Mike Tyson`,
+         name4: `Lionel Messi`,
+         name5: `Elon Musk`,
+         name6: `Bill Gates`
+      }
+   }
+]
+console.log(someMoreMoreMoreThing);
